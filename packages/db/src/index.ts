a4f8771d9552db9d7eb2c7a32f84f7984e2641ba
vanesarossi61 <vanesarossi61@gmail.com>
2026-03-09 19:45:49 -0300

    feat: initial monorepo setup - Phase 1 foundation

---
+++ packages/db/src/index.ts
@@ -0,0 +1,8 @@
+export { PrismaClient } from '@prisma/client'
+export type {
+  User,
+  Profile,
+  SensoryPreferences,
+  SocialBattery,
+} from '@prisma/client'
+export { BatteryLevel, ToneTag } from '@prisma/client'
