10a337052d56dac54b2270e4c4c186bdf49bb1f6
vanesarossi61 <vanesarossi61@gmail.com>
2026-03-09 19:43:50 -0300

    feat: initial monorepo setup - Phase 1 foundation

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: [
+    "node_modules/",
+    "dist/",
+    ".next/",
+    "*.config.js",
+    "*.config.ts",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_" },
+    ],
+    "@typescript-eslint/no-explicit-any": "warn",
+  },
+};
