960e6f85d657c74963f36fd945212c21d03af6fe
vanesarossi61 <vanesarossi61@gmail.com>
2026-03-10 00:13:24 -0300

    feat(phase3): add Feed & Posts system with onboarding flow

--- apps/web/src/lib/bionicReading.ts
+++ apps/web/src/lib/bionicReading.ts
@@ -111,6 +111,6 @@ function escapeHtml(text: string): string {
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
-    .replace(/\"/g, '&quot;')
+    .replace(/"/g, '&quot;')
     .replace(/'/g, '&#39;')
 }
